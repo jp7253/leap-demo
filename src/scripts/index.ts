9830d7cc54fe029ac14b1febc75584d26e1968d3
John Perdue <johnperdue@Johns-MacBook-Pro.local>
2017-04-20 11:34:05 +0100

    Simplify rotation logic.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -47,6 +47,8 @@ function oneHands(scene: Scene3d, hand: any): void {
     // scene.model.rotation.x = rotationX;
 }
 
+
+let avgDist :number; 
 function twoHands(scene: Scene3d, leapFrame :any) {
 
     let leftHandIndex = leapFrame.hands[0].type == 'left' ? 0 : 1;
@@ -60,15 +62,18 @@ function twoHands(scene: Scene3d, leapFrame :any) {
     let rightHandPos = Leap.vec3.fromValues(rightHand.palmPosition[0], rightHand.palmPosition[1], rightHand.palmPosition[2]);
     let dist: number = Leap.vec3.dist(leftHandPos, rightHandPos);
 
+
     //Set the camera location accordingly
-    scene.camera.position.z = Utils.Map(dist, 500, 10, 5, 70);
+    scene.camera.position.z = Utils.Map((dist + avgDist)/2, 500, 10, 5, 70);
+
+    avgDist = dist;
 
     //Get the vector between the hands and set rotation from that
-    let vector = Leap.vec3.create();
-    Leap.vec3.sub(vector, leftHandPos, rightHandPos);
+    //let vector = Leap.vec3.create();
+    //Leap.vec3.sub(vector, leftHandPos, rightHandPos);
 
-    let rotationZ = Utils.Map(vector[2], -300, 300, -1.5, 1.5);
-    let rotationY = Utils.Map(vector[1], -300, 300, -1.5, 1.5);
+    let rotationZ = Utils.Map(rightHandPos[2], 300, -300, -3, 6);
+    //let rotationY = Utils.Map(rightHandPos[1], -1000, 3000, -3, 9);
     scene.model.rotation.z = (scene.model.rotation.z + rotationZ) /2;
-    scene.model.rotation.y = (scene.model.rotation.y + rotationY) /2;
+    //scene.model.rotation.y = (scene.model.rotation.y + rotationY) /2;
 }
